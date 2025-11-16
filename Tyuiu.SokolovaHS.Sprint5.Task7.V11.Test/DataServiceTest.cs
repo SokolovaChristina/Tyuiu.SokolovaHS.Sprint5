a4c8aa4b06fc82d4ed69d170eb66c9ed00ca6518
SokolovaChristina <Tyuiu.SokolovaHS@outlook.com>
2025-11-17 03:33:09 +0500

    Task7

--- Tyuiu.SokolovaHS.Sprint5.Task7.V11.Test/DataServiceTest.cs
+++ Tyuiu.SokolovaHS.Sprint5.Task7.V11.Test/DataServiceTest.cs
@@ -13,14 +13,14 @@ namespace Tyuiu.SokolovaHS.Sprint5.Task7.V11.Test
         {
             string inputPath = @"test_input.txt";
 
-            // Создаем тестовый файл, который должен дать "П,?О.О."
+            // Создаем тестовый файл, который должен дать "П,? О. О ."
             File.WriteAllText(inputPath, "Привет, ? мир. это тест .", Encoding.UTF8);
 
             DataService ds = new DataService();
             string outputPath = ds.LoadDataAndSave(inputPath);
 
             string result = File.ReadAllText(outputPath, Encoding.UTF8);
-            string expected = "П,?О.О.";
+            string expected = "П,? О. О .";
 
             Assert.AreEqual(expected, result);
 
@@ -29,36 +29,18 @@ namespace Tyuiu.SokolovaHS.Sprint5.Task7.V11.Test
         }
 
         [TestMethod]
-        public void CheckedSpacesRemoved()
+        public void CheckedMultipleDots()
         {
             string inputPath = @"test_input2.txt";
 
-            File.WriteAllText(inputPath, "один   два    три", Encoding.UTF8);
+            // Тест с несколькими точками
+            File.WriteAllText(inputPath, "первый . второй . третий .", Encoding.UTF8);
 
             DataService ds = new DataService();
             string outputPath = ds.LoadDataAndSave(inputPath);
 
             string result = File.ReadAllText(outputPath, Encoding.UTF8);
-            string expected = "";
-
-            Assert.AreEqual(expected, result);
-
-            File.Delete(inputPath);
-            File.Delete(outputPath);
-        }
-
-        [TestMethod]
-        public void CheckedAllLowerCaseRemoved()
-        {
-            string inputPath = @"test_input3.txt";
-
-            File.WriteAllText(inputPath, "абвгдеёжзийклмнопрстуфхцчшщъыьэюя", Encoding.UTF8);
-
-            DataService ds = new DataService();
-            string outputPath = ds.LoadDataAndSave(inputPath);
-
-            string result = File.ReadAllText(outputPath, Encoding.UTF8);
-            string expected = "";
+            string expected = " . . .";
 
             Assert.AreEqual(expected, result);
 
