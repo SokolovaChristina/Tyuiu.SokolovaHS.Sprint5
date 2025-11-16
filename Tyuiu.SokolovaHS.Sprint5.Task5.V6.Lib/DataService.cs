d25769265ef31826332b08e26cd2d9879493d4c6
SokolovaChristina <Tyuiu.SokolovaHS@outlook.com>
2025-11-17 02:24:47 +0500

    Task5

--- Tyuiu.SokolovaHS.Sprint5.Task5.V6.Lib/DataService.cs
+++ Tyuiu.SokolovaHS.Sprint5.Task5.V6.Lib/DataService.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Globalization;
 using System.IO;
+using System.Globalization;
 using System.Linq;
-using System.Text;
 using tyuiu.cources.programming.interfaces.Sprint5;
 
 namespace Tyuiu.SokolovaHS.Sprint5.Task5.V6.Lib
@@ -11,41 +10,27 @@ namespace Tyuiu.SokolovaHS.Sprint5.Task5.V6.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            // Версия, которая дает результат близкий к 6.997
-            // Предполагаем, что в файле числа, которые в среднем дают ~6.997
-
+            // Читаем весь файл
             string allText = File.ReadAllText(path);
 
-            // Агрессивная очистка - оставляем только цифры, точки и минусы
-            var numbersOnly = new string(allText
-                .Where(c => char.IsDigit(c) || c == '.' || c == '-' || c == ',')
-                .ToArray())
-                .Replace(',', '.'); // Нормализуем разделители
+            // Нормализуем - заменяем запятые на точки
+            allText = allText.Replace(',', '.');
 
-            // Вставляем пробелы между числами
-            var withSpaces = new StringBuilder();
-            for (int i = 0; i < numbersOnly.Length; i++)
-            {
-                withSpaces.Append(numbersOnly[i]);
-                // Если следующий символ - цифра или минус, а текущий - точка, или наоборот
-                if (i < numbersOnly.Length - 1 &&
-                    ((numbersOnly[i] == '.' && char.IsDigit(numbersOnly[i + 1])) ||
-                     (char.IsDigit(numbersOnly[i]) && numbersOnly[i + 1] == '-') ||
-                     (numbersOnly[i] == '.' && numbersOnly[i + 1] == '-')))
-                {
-                    withSpaces.Append(' ');
-                }
-            }
-
-            string[] numberStrings = withSpaces.ToString()
-                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            // Разбиваем на числа по пробелам
+            string[] numberStrings = allText.Split(
+                new[] { ' ', '\t', '\r', '\n' },
+                StringSplitOptions.RemoveEmptyEntries
+            );
 
             double sum = 0;
             int count = 0;
 
             foreach (string numStr in numberStrings)
             {
-                if (double.TryParse(numStr, NumberStyles.Float, CultureInfo.InvariantCulture, out double number))
+                // Убираем лишние пробелы
+                string cleanNumStr = numStr.Trim();
+
+                if (double.TryParse(cleanNumStr, NumberStyles.Float, CultureInfo.InvariantCulture, out double number))
                 {
                     sum += number;
                     count++;
